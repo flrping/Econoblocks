e6d8115303f3d46595d2405d2577abf9b219551c
flrp <flrping7@gmail.com>
2024-05-04 07:12:41 -0400

    Update EconoblocksExpansion.java
    2.0.0 is ready to push.

--- src/main/java/dev/flrp/econoblocks/placeholder/EconoblocksExpansion.java
+++ src/main/java/dev/flrp/econoblocks/placeholder/EconoblocksExpansion.java
@@ -1,6 +1,8 @@
 package dev.flrp.econoblocks.placeholder;
 
 import dev.flrp.econoblocks.Econoblocks;
+import dev.flrp.econoblocks.util.multiplier.MultiplierGroup;
+import dev.flrp.econoblocks.util.multiplier.MultiplierProfile;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -38,51 +40,86 @@ public class EconoblocksExpansion extends PlaceholderExpansion {
     }
 
     @Override
-    public String onPlaceholderRequest(Player player, @NotNull String subject) {
+    public String onPlaceholderRequest(Player player, @NotNull String placeholder) {
         if(plugin.getMultiplierManager().getMultiplierProfile(player.getUniqueId()) == null) {
             return "1.0";
         }
-        String[] params = subject.split("_");
+        String[] params = placeholder.split("_");
+        StringBuilder value = new StringBuilder(params[2]);
+        MultiplierProfile profile = plugin.getDatabaseManager().getMultiplierProfile(player.getUniqueId());
+        MultiplierGroup group = plugin.getMultiplierManager().getMultiplierGroup(player.getUniqueId());
+        if(params.length > 3) {
+            for(int i = 3; i < params.length; i++) {
+                value.append("_").append(params[i]);
+            }
+        }
         switch (params[1]) {
             case "block":
-                return requestBlockPlaceholder(player, params[2]);
+                return requestBlockPlaceholder(value.toString(), profile, group);
             case "tool":
-                return requestToolPlaceholder(player, params[2]);
+                return requestToolPlaceholder(value.toString(), profile, group);
             case "world":
-                return requestWorldPlaceholder(player, params[2]);
+                return requestWorldPlaceholder(value.toString(), profile, group);
             default:
                 return "";
         }
     }
 
-    private String requestBlockPlaceholder(Player player, String subject) {
+    private String requestBlockPlaceholder(String value, MultiplierProfile profile, MultiplierGroup group) {
+        Material material = null;
+        try {
+            material = Material.valueOf(value.toUpperCase());
+        } catch (IllegalArgumentException ignored) {}
+        if(material != null) {
+            if(profile.getMaterials().containsKey(material)) {
+                return profile.getMaterials().get(material).toString();
+            } else
+            if(group != null && group.getMaterials().containsKey(material)) {
+                return group.getMaterials().get(material).toString();
+            }
+        } else {
+            if(profile.getCustomMaterials().containsKey(value)) {
+                return profile.getCustomMaterials().get(value).toString();
+            } else
+            if(group != null && group.getCustomMaterials().containsKey(value)) {
+                return group.getCustomMaterials().get(value).toString();
+            }
+        }
+        return "1.0";
+    }
+
+    private String requestToolPlaceholder(String subject, MultiplierProfile profile, MultiplierGroup group) {
         Material material = null;
         try {
             material = Material.valueOf(subject.toUpperCase());
         } catch (IllegalArgumentException ignored) {}
         if(material != null) {
-            return String.valueOf(plugin.getMultiplierManager().getMultiplierProfile(player.getUniqueId()).getMaterials().getOrDefault(material, 1.0));
+            if(profile.getTools().containsKey(material)) {
+                return profile.getTools().get(material).toString();
+            } else
+            if(group != null && group.getTools().containsKey(material)) {
+                return group.getTools().get(material).toString();
+            }
         } else {
-            return String.valueOf(plugin.getMultiplierManager().getMultiplierProfile(player.getUniqueId()).getCustomMaterials().getOrDefault(subject, 1.0));
+            if(profile.getCustomTools().containsKey(subject)) {
+                return profile.getCustomTools().get(subject).toString();
+            } else
+            if(group != null && group.getCustomTools().containsKey(subject)) {
+                return group.getCustomTools().get(subject).toString();
+            }
         }
+        return "1.0";
     }
 
-    private String requestToolPlaceholder(Player player, String subject) {
-        Material material = null;
-        try {
-            material = Material.valueOf(subject.toUpperCase());
-        } catch (IllegalArgumentException ignored) {}
-        if(material != null) {
-            return String.valueOf(plugin.getMultiplierManager().getMultiplierProfile(player.getUniqueId()).getTools().getOrDefault(material, 1.0));
-        } else {
-            return String.valueOf(plugin.getMultiplierManager().getMultiplierProfile(player.getUniqueId()).getCustomTools().getOrDefault(subject, 1.0));
-        }
-    }
-
-    private String requestWorldPlaceholder(Player player, String subject) {
+    private String requestWorldPlaceholder(String subject, MultiplierProfile profile, MultiplierGroup group) {
         try {
             if(Bukkit.getWorld(UUID.fromString(subject)) != null) {
-                return String.valueOf(plugin.getMultiplierManager().getMultiplierProfile(player.getUniqueId()).getWorlds().getOrDefault(UUID.fromString(subject), 1.0));
+                if(profile.getWorlds().containsKey(UUID.fromString(subject))) {
+                    return profile.getWorlds().get(UUID.fromString(subject)).toString();
+                } else
+                if(group != null && group.getWorlds().containsKey(UUID.fromString(subject))) {
+                    return group.getWorlds().get(UUID.fromString(subject)).toString();
+                }
             }
         } catch (IllegalArgumentException ignored) {}
         return "1.0";
